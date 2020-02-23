f50dfbef9bf707d2dc6496e6f6cf9a95fcf06675
Peter Dinda <pdinda@northwestern.edu>
2020-02-23 09:22:23 -0600

    Add watchdog interface and mechanism
    This is currently implemented using the i8254 PIT and the
    first IOAPIC to send an NMI to cpu 0.   The watchdog is
    petted by interrupt entry code.
    
    Because the i8254 cannot manage longer intervals than about
    54 ms, you need to be sure the watchdog is petted more frequently
    than this.   Note that with the default 10 Hz timer rate, this is
    not the case.

--- src/arch/x64/init.c
+++ src/arch/x64/init.c
@@ -67,6 +67,10 @@
 #include <nautilus/cmdline.h>
 #include <test/test.h>
 
+#ifdef NAUT_CONFIG_WATCHDOG
+#include <nautilus/watchdog.h>
+#endif
+
 #ifdef NAUT_CONFIG_ASPACES
 #include <nautilus/aspace.h>
 #endif
@@ -541,6 +545,10 @@ init (unsigned long mbd,
     nk_run_tests(naut);
 #endif
 
+#ifdef NAUT_CONFIG_WATCHDOG
+    nk_watchdog_init(NAUT_CONFIG_WATCHDOG_DEFAULT_TIME_MS * 1000000UL);
+#endif
+    
     nk_launch_shell("root-shell",0,0,0);
 
     runtime_init();
